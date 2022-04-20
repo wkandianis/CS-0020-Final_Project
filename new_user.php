07f03914fe71a0c4c03e06076b39b6e6f8ef0514
wkandianis <60202933+wkandianis@users.noreply.github.com>
2022-04-20 18:26:45 -0400

    Add files via upload

---
+++ new_user.php
@@ -0,0 +1,36 @@
+
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        $username = $_POST['uname'];
+        $password = $_POST['pwd'];
+        $location = $_POST['loc'];
+
+        $server = "localhost";   
+        $userid = "ukgxpyvvigex4";
+        $pw = "$1njfkiz312";
+        $db = "dbbc8pf3dpkksh";
+        // Create connection
+        $conn = new mysqli($server, $userid, $pw);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $conn->select_db($db);
+
+        $sql = "INSERT INTO users (username, pwd, loc)
+        VALUES ('$username','$password', '$location')";
+        
+    
+        $result = $conn->query($sql);
+        if ($result === TRUE) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
+?>
